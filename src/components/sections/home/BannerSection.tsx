1c2f6da7e25a979078b62976af01e0593fa28787
Manikandan <manikandan@capecomsolutions.com>
2025-11-13 17:01:31 +0530

    changes

--- src/components/sections/home/BannerSection.tsx
+++ src/components/sections/home/BannerSection.tsx
@@ -24,9 +24,9 @@ export default function HomeContent() {
     return () => ctx.revert();
   }, []);
   return (
-    <div className="relative min-h-screen w-full overflow-hidden scroll-smooth">
+    <div className="relative min-h-screen w-full overflow-hidden">
      <BackgroundVideo />
-      <section  ref={sectionRef} className="z-10 text-white  px-9 sm:px-8 lg:px-52 pt-[130px]">
+      <section  ref={sectionRef} className="z-10 text-white  px-6 md:px-12 lg:px-52 pt-[130px]">
         <h1 className="hero-title text-[38px] font-400 sm:text-[48px] md:text-[72px] lg:text-[80px] leading-[45px] sm:leading-[55px] md:leading-[85px] lg:leading-[88px] font-urbanist font-regular mix-blend-difference max-w-[95%] sm:max-w-[85%] md:max-w-[75%] lg:max-w-[1100px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
         </h1>
--- src/components/sections/home/background-video.tsx
+++ src/components/sections/home/background-video.tsx
@@ -1,6 +1,6 @@
 export default function BackgroundVideo() {
   return (
-   <div className="absolute inset-0 -z-10">
+   <div className="absolute inset-0 -z-10 ">
         <video
           autoPlay
           loop
