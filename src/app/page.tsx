d5aa6875b25da8ca2d66d28b66ae35ba48c9f37f
Manikandan <manikandan@capecomsolutions.com>
2025-11-14 12:19:45 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import BannerSection from "@/components/sections/home/BannerSection"
 import CampusSection from "@/components/sections/home/campus-section"
 import CourseSection from "@/components/sections/home/CourseSection"
 import FacultySection from "@/components/sections/home/faculty-section"
-import FooterSection from "@/components/sections/home/footer-section"
 import LifeSection from "@/components/sections/home/life-section"
 import SponsorSection from "@/components/sections/home/sponsor-section"
 import TestimonialsSection from "@/components/sections/home/testimonial-section"
@@ -18,7 +17,6 @@ const HomePage = () => {
        <LifeSection />
        <TestimonialsSection />
        <SponsorSection />
-       <FooterSection />
        </>
   )
 }
