a101d29949828ec7dea8ed87a732eaf00881d2ac
pratheesh <pratheeshngl98@gmail.com>
2025-11-16 00:27:27 +0530

    update footer

--- src/components/layouts/WebFooter.tsx
+++ src/components/layouts/WebFooter.tsx
@@ -1,7 +1,20 @@
+import { FooterBg } from "@/helpers/ImageHelper";
+import ContainerWidget from "../widgets/ContainerWidget";
+
 const WebFooter = () => {
+  const bgImageUrl =
+    typeof FooterBg === "string" ? FooterBg : FooterBg?.src || FooterBg;
+
   return (
-    <footer className="w-full">
-      <div className="container mx-auto px-4 py-4">Footer</div>
+    <footer
+      className="w-full bg-cover bg-center bg-no-repeat h-[1100px] bg-black"
+      style={{
+        backgroundImage: `url(${bgImageUrl})`,
+      }}
+    >
+      <ContainerWidget>
+        <div className="container mx-auto px-4 py-4 text-white">Footer</div>
+      </ContainerWidget>
     </footer>
   );
 };
--- src/components/sections/home/TestimonialSection.tsx
+++ src/components/sections/home/TestimonialSection.tsx
@@ -1,7 +1,11 @@
+import ContainerWidget from "@/components/widgets/ContainerWidget";
+
 const TestimonialSection = () => {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      TestimonialSection
+    <section className="w-full bg-[#ECECEC] h-screen flex items-center justify-center">
+      <ContainerWidget>
+        <div className="text-black text-center">TestimonialSection</div>
+      </ContainerWidget>
     </section>
   );
 };
