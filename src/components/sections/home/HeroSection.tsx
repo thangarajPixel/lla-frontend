ec63118f015adac75cab2f068803a0eba939fb06
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-17 13:20:45 +0530

    update hero section

--- src/components/sections/home/HeroSection.tsx
+++ src/components/sections/home/HeroSection.tsx
@@ -1,55 +1,10 @@
 "use client";
 
-import { gsap } from "gsap";
-import { useEffect, useRef, useState } from "react";
 import ContainerWidget from "@/components/widgets/ContainerWidget";
 import ParallaxWidget from "@/components/widgets/ParallaxWidget";
 import ScrollWidget from "@/components/widgets/ScrollWidget";
 
 const HeroSection = () => {
-  const contentArray = [
-    "Transform your future through learning experiences.",
-    "Join our community where knowledge meets opportunity.",
-    "Build your career with expert guidance and mentorship.",
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const textRef = useRef<HTMLHeadingElement>(null);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (textRef.current) {
-        gsap.to(textRef.current, {
-          opacity: 0,
-          y: -70,
-          duration: 0.8,
-          ease: "power2.in",
-          onComplete: () => {
-            setCurrentIndex(
-              (prevIndex) => (prevIndex + 1) % contentArray.length,
-            );
-          },
-        });
-      }
-    }, 8000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const currentText = contentArray[currentIndex];
-    if (textRef.current && currentText) {
-      gsap.set(textRef.current, { opacity: 0, y: 70 });
-      gsap.to(textRef.current, {
-        opacity: 1,
-        y: 0,
-        duration: 2.5,
-        delay: 0.3,
-        ease: "power3.out",
-      });
-    }
-  }, [currentIndex]);
-
   return (
     <section className="relative w-full h-screen overflow-hidden">
       <ParallaxWidget speed={0.5} className="absolute inset-0 w-full h-full">
@@ -72,10 +27,9 @@ const HeroSection = () => {
           <div className="flex items-center justify-center mt-40 md:mt-[180px] xl:mt-[150px] 2xl:mt-[140px] 3xl:mt-[150px]">
             <ContainerWidget>
               <h1
-                ref={textRef}
-                className="font-urbanist font-normal mb-6 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-[85px] 3xl:text-[112px] max-w-[850] 3xl:max-w-[1120px]"
+                className="font-urbanist leading-24 font-normal mb-6 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-[85px] 3xl:text-[112px] max-w-[850] 3xl:max-w-[1120px]"
               >
-                {contentArray[currentIndex]}
+               Where Light Inspires the Art of Seeing
               </h1>
             </ContainerWidget>
           </div>
