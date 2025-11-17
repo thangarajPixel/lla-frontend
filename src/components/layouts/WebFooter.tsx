afab4afa88cd86a53d89f0987cd8cfc3496a6fbb
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-17 12:13:38 +0530

    updates

--- src/components/layouts/WebFooter.tsx
+++ src/components/layouts/WebFooter.tsx
@@ -16,22 +16,19 @@ import LinkWidget from "../widgets/LinkWidget";
 import OrangeButtonWidget from "../widgets/OrangeButtonWidget";
 
 const WebFooter = () => {
-  const bgImageUrl =
-    typeof FooterBg === "string" ? FooterBg : FooterBg?.src || FooterBg;
-
   return (
     <footer
       className="w-full bg-cover bg-bottom bg-no-repeat min-h-screen bg-black text-white py-8 md:py-12 lg:py-22"
       style={{
-        backgroundImage: `url(${bgImageUrl})`,
+        backgroundImage: `url(${FooterBg?.src})`,
       }}
     >
       <ContainerWidget>
         <div className="flex flex-col items-start justify-start gap-5 md:gap-6">
-          <h6 className="text-[32px] md:text-[40px] lg:text-[50px] 3xl:text-[56px] font-normal font-urbanist">
+          <h6 className="text-[32px] md:text-[40px] lg:text-[50px] 3xl:text-[56px] font-normal font-urbanist leading-10">
             Let Your Passion Shine Through
           </h6>
-          <p className="text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[24px] font-normal max-w-full md:max-w-[550px]">
+          <p className="text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[24px] font-normal max-w-full md:max-w-[550px]">
             Step into a world where every click, every frame, and every
             flashlight is the path to your creative future.
           </p>
@@ -43,7 +40,7 @@ const WebFooter = () => {
               <ImageWidget
                 src={FooterLogo}
                 alt="Logo"
-                className="w-48 md:w-65 h-auto"
+                className="w-65 h-auto"
               />
               <div className="flex flex-row items-start justify-start gap-6">
                 <div className="flex flex-row gap-2">
@@ -55,13 +52,13 @@ const WebFooter = () => {
                     />
                   </div>
                   <div>
-                    <p className="text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
+                    <p className="text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
                       Light & Life Academy,
                     </p>
-                    <p className="text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
+                    <p className="text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
                       Lovedale, Ooty,
                     </p>
-                    <p className="text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
+                    <p className="text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal">
                       Tamil Nadu - 643 003.
                     </p>
                   </div>
@@ -98,13 +95,13 @@ const WebFooter = () => {
                 </li>
               </ul>
             </div>
-
+            <div className="block md:hidden border-b border-white opacity-30 w-full h-px mt-3" />
             <div className="flex flex-row md:hidden gap-6 w-full">
               <div className="flex flex-col items-start justify-start gap-3 flex-1">
                 <h6 className="text-[18px] md:text-[20px] 3xl:text-[24px] font-normal font-urbanist">
                   Quick Links
                 </h6>
-                <ul className="flex flex-col items-start justify-start gap-2 text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
+                <ul className="flex flex-col items-start justify-start gap-2 text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
                   <li>
                     <LinkWidget href="/">Home</LinkWidget>
                   </li>
@@ -130,7 +127,7 @@ const WebFooter = () => {
                 <h6 className="text-[18px] md:text-[20px] 3xl:text-[24px] font-normal font-urbanist">
                   Courses
                 </h6>
-                <ul className="flex flex-col items-start justify-start gap-2 text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
+                <ul className="flex flex-col items-start justify-start gap-2 text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
                   <li className="max-w-full md:max-w-[250px]">
                     <LinkWidget href="/">
                       PG Diploma in Professional Photography & Digital Production
@@ -149,7 +146,7 @@ const WebFooter = () => {
               <h6 className="text-[18px] md:text-[20px] 3xl:text-[24px] font-normal font-urbanist">
                 Quick Links
               </h6>
-              <ul className="flex flex-col items-start justify-start gap-2 text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
+              <ul className="flex flex-col items-start justify-start gap-2 text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
                 <li>
                   <LinkWidget href="/">Home</LinkWidget>
                 </li>
@@ -175,7 +172,7 @@ const WebFooter = () => {
               <h6 className="text-[18px] md:text-[20px] 3xl:text-[24px] font-normal font-urbanist">
                 Courses
               </h6>
-              <ul className="flex flex-col items-start justify-start gap-2 text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
+              <ul className="flex flex-col items-start justify-start gap-2 text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
                 <li className="max-w-full md:max-w-[250px]">
                   <LinkWidget href="/">
                     PG Diploma in Professional Photography & Digital Production
@@ -193,7 +190,7 @@ const WebFooter = () => {
               <h6 className="text-[18px] md:text-[20px] 3xl:text-[24px] font-normal font-urbanist">
                 Resources
               </h6>
-              <ul className="flex flex-col items-start justify-start gap-2 text-[14px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
+              <ul className="flex flex-col items-start justify-start gap-2 text-[16px] md:text-[16px] lg:text-[14px] 3xl:text-[18px] font-normal leading-7">
                 <li>
                   <LinkWidget href="/">Blog</LinkWidget>
                 </li>
@@ -205,21 +202,33 @@ const WebFooter = () => {
           </div>
           <div className="border-b border-white opacity-30 w-full h-px mt-3" />
 
-          <div className="flex flex-col sm:flex-row items-start sm:items-center justify-start gap-4 sm:gap-6 md:gap-23 py-3 w-full">
+          <div className="flex flex-wrap sm:flex-nowrap items-start sm:items-center justify-center sm:justify-start gap-4 sm:gap-6 md:gap-23 py-3 pb-1 w-full">
             <ImageWidget
               src={LlaOutreach}
               alt="LlaOutreach"
               className="w-40 md:w-60 h-auto"
             />
-            <ImageWidget src={IQAC} alt="IQAC" className="w-24 md:w-30 h-auto" />
+            <ImageWidget 
+              src={IQAC} 
+              alt="IQAC" 
+              className="w-24 md:w-30 h-auto" 
+            />
+            <div className="w-full sm:hidden flex justify-center">
+              <ImageWidget
+                src={LlaOnline}
+                alt="LlaOnline"
+                className="w-36 md:w-50 h-auto"
+              />
+            </div>
             <ImageWidget
               src={LlaOnline}
               alt="LlaOnline"
-              className="w-36 md:w-50 h-auto"
+              className="hidden sm:block w-36 md:w-50 h-auto"
             />
           </div>
-          <p className="text-[12px] md:text-[14px] lg:text-[14px] 3xl:text-[18px] font-normal max-w-full md:max-w-[350px]">
-            © 2025 Light & Life Academy, Premier College for Professional
+          <div className="block md:hidden border-b border-white opacity-30 w-full h-px mt-3" />
+          <p className="text-center md:text-left text-[16px] md:text-[14px] lg:text-[14px] 3xl:text-[18px] font-normal max-w-full md:max-w-[350px]">
+            © {new Date().getFullYear()} Light & Life Academy, Premier College for Professional
             Photography in India.All rights reserved.
           </p>
         </div>
