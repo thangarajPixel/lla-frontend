40c4cc854a818fcc1a30d8d398037a0d30f3ace4
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-18 12:30:15 +0530

    updates

--- src/components/sections/home/LifeSection.tsx
+++ src/components/sections/home/LifeSection.tsx
@@ -3,7 +3,7 @@ import { Life } from "@/helpers/ImageHelper";
 const LifeSection = () => {
   return (
     <section
-      className="w-full h-[1100px] flex items-center justify-center bg-contain bg-center bg-no-repeat"
+      className="w-full flex items-center justify-center bg-contain bg-center bg-no-repeat"
       style={{
         backgroundImage: `url(${Life.src})`,
       }}
