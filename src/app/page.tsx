81e0bb4e2c2beac47a54c5b87a65c291a345bca6
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-19 11:24:27 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,12 @@ import LifeSection from "@/components/sections/home/LifeSection";
 import SponsorsSection from "@/components/sections/home/SponsorsSection";
 import StudentSection from "@/components/sections/home/StudentSection";
 import TestimonialSection from "@/components/sections/home/TestimonialSection";
+// import { getLandingPageData } from "./api/server";
 
-const Home = () => {
+const Home = async () => {
+
+  // const response = await getLandingPageData();
+
   return (
     <Fragment>
       <HeroSection />
