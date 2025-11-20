a2dace9f757109a23b0426692f4483f2a62df5a0
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-20 10:38:51 +0530

    updates

--- src/components/sections/home/CampusSection.tsx
+++ src/components/sections/home/CampusSection.tsx
@@ -6,7 +6,7 @@ import { Building } from "@/helpers/ImageHelper";
 const CampusSection = () => {
   return (
     <section
-      className="w-full h-screen bg-fixed bg-cover bg-bottom bg-no-repeat py-20"
+      className="w-full h-screen bg-fixed bg-cover bg-bottom bg-no-repeat sm:py-20 py-10"
       style={{
         backgroundImage: `url(${Building.src})`,
       }}
@@ -14,11 +14,11 @@ const CampusSection = () => {
       <ContainerWidget>
         <ScrollWidget animation="slideRight">
           <div className="space-y-6 md:space-y-8 lg:space-y-10 xl:space-y-12 2xl:space-y-14 3xl:space-y-16">
-            <div className="space-y-2 md:space-y-3 lg:space-y-5">
+            <div className="space-y-3.5 md:space-y-3 lg:space-y-5">
               <h3 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 3xl:text-[80px] font-normal text-black font-urbanist">
                 Campus
               </h3>
-              <p className="font-area-variable font-semibold text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-[40px] text-black">
+              <p className="font-area-variable font-semibold text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-[40px] text-black">
                 Deliberately Designed,
                 <span className="text-[#E97451] ml-2">Endlessly Inspiring</span>
               </p>
--- src/components/sections/home/CourseSection.tsx
+++ src/components/sections/home/CourseSection.tsx
@@ -14,14 +14,14 @@ const CourseSection = () => {
             <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 3xl:text-[80px] font-normal text-black font-urbanist">
               Courses
             </h2>
-            <p className="font-area-variable font-semibold text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-[40px] text-black">
+            <p className="font-area-variable font-semibold text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-[40px] text-black">
               Designed For
               <span className="text-[#E97451] ml-2">Immersive Learning</span>
             </p>
             <p className="text-[16px] lg:text-[15px] 3xl:text-[18px] font-normal text-black leading-normal">
               Here, learning is deliberate and layered, aimed at building strong
               conceptual foundations.
-              <br /> Where skills are honed, ideas take shape, and observation
+              <br className="hidden sm:block" /> Where skills are honed, ideas take shape, and observation
               becomes instinct.
             </p>
           </div>
