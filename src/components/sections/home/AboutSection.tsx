a83245ee99983fca779b1aa73fcd7f4ca8d9b88d
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-20 12:13:55 +0530

    updates

--- src/components/sections/home/AboutSection.tsx
+++ src/components/sections/home/AboutSection.tsx
@@ -1,11 +1,13 @@
 import ContainerWidget from "@/components/widgets/ContainerWidget";
 import OrangeButtonWidget from "@/components/widgets/OrangeButtonWidget";
+import ImageWidget from "@/components/widgets/ImageWidget";
+import { Dummy1 } from "@/helpers/ImageHelper";
 
 const AboutSection = () => {
   return (
-    <section className="w-full h-screen bg-[#ECECEC] flex items-center justify-center z-40 relative">
+    <section className="w-full bg-[#ECECEC] flex items-center justify-center z-40 relative pt-[300px] pb-60">
       <ContainerWidget>
-        <div className="space-y-6 md:space-y-8 lg:space-y-10 xl:space-y-12 2xl:space-y-14 3xl:space-y-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-12 xl:gap-14 2xl:gap-16">
           <div className="space-y-3.5 md:space-y-3 lg:space-y-5">
             <h3 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 3xl:text-[80px] font-normal text-black font-urbanist">
               About LLA
@@ -16,11 +18,16 @@ const AboutSection = () => {
             </p>
             <p className="text-[16px] lg:text-[15px] 3xl:text-[18px] font-normal text-black leading-normal w-full md:max-w-[450px]">
               In thought, in learning, and in spirit, what began as Iqbal
-              Mohamed’s dream to establish India’s first professional
+              Mohamed's dream to establish India's first professional
               photography institute has grown into a community that continues to
               explore, question, and create with purpose.
             </p>
             <OrangeButtonWidget content="Step into the LLA" />
+          </div>
+          <div>
+             <ImageWidget src={Dummy1} alt="About 3" className="absolute w-[300px] h-[250px] mt-[-200px] ml-[150px] object-cover opacity-40" />
+             <ImageWidget src={Dummy1} alt="About 1"  className="w-[500px] h-[350px] ml-[-100px] object-cover relative z-10" />
+             <ImageWidget src={Dummy1} alt="About 2"  className="absolute w-[420px] h-[280px] mt-[-150px] ml-[80px] object-cover opacity-40" />
           </div>
         </div>
       </ContainerWidget>
