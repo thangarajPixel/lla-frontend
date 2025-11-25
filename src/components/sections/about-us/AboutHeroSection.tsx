c9ee610af9a9bd12df64d6df92886341633d90e1
Manikandan <manikandan@capecomsolutions.com>
2025-11-25 18:17:07 +0530

    changes

--- src/components/sections/about-us/AboutHeroSection.tsx
+++ src/components/sections/about-us/AboutHeroSection.tsx
@@ -2,22 +2,34 @@ import { Fragment } from "react/jsx-runtime";
 import ContainerWidget from "@/components/widgets/ContainerWidget";
 import ScrollWidget from "@/components/widgets/ScrollWidget";
 import { getS3Url } from "@/helpers/ConstantHelper";
-import { AboutBg } from "@/helpers/ImageHelper";
 import type { AboutHeroSectionProps } from "./utils/about-us";
 
 const AboutHeroSection = ({ data }: AboutHeroSectionProps) => {
   return (
     <Fragment>
-      <section className="w-full bg-white h-auto sm:min-h-[300px] md:min-h-[250px] lg:min-h-[260px] xl:min-h-[250px] 2xl:min-h-[320px] 3xl:min-h-[320px]">
+      <section className="
+          z-50 max-w-[2560px] bg-cover bg-bottom bg-no-repeat 
+          min-h-[2150px] 
+          md:min-h-[1200px]
+          lg:min-h-[1200px]
+          xl:min-h-[1100px] 
+          2xl:min-h-[1550px]
+          3xl:min-h-[1650px]
+          bg-white text-white
+        "
+        style={{
+          backgroundImage: `url(${getS3Url(data?.Image?.url)})`,
+        }}
+      >
         <ContainerWidget>
-          <div className="flex flex-col items-start justify-start gap-2 md:gap-2">
-            <ScrollWidget animation="fadeUp" delay={0.1}>
+           <ScrollWidget animation="fadeUp" delay={0.1}>
               <h3  className="text-[32px] sm:text-[34px] md:text-[36px] lg:text-[48px] 
                 xl:text-[56px] 2xl:text-[64px] 3xl:text-[64px] 4xl:text-[64px]
                 font-regular text-black font-urbanist">
                 {data.Title}
               </h3>
             </ScrollWidget>
+          <div className="flex flex-col items-start justify-start gap-2 md:gap-2">
             <ScrollWidget animation="slideLeft" delay={0.2}>
               <p  className="font-mulish font-regular text-[24px] md:text-[28px] lg:text-[28px] xl:text-[24px] 
                 2xl:text-[30px] 3xl:text-[40px] text-black sm:max-w-[600px] md:max-w-[750px] lg:max-w-[800px] 
@@ -26,67 +38,47 @@ const AboutHeroSection = ({ data }: AboutHeroSectionProps) => {
                 <span className="text-[#E97451] ml-2">{data.SubHeading}</span>
               </p>
             </ScrollWidget>
-            <ScrollWidget animation="fadeUp" delay={0.3}>
+             <ScrollWidget animation="fadeUp" delay={0.3}>
               <p className="text-[16px] sm:text-[16px] lg:text-[16px] 
-                xl:text-[16px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
-                font-regular text-black leading-[24px]   md:leading-[26px]
-                w-full md:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px] 
+                xl:text-[14px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
+                font-regular text-black leading-[24px]   md:leading-[20px] lg:leading-[20px] xl:leading-[20px] 3xl:leading-[26px]
+                w-full md:max-w-[650px] xl:max-w-[660px] 2xl:max-w-[850px] 
                 3xl:max-w-[854px] 4xl:max-w-[1100px]">
                 {data.Description[0].children[0].text}
               </p>
             </ScrollWidget>
-          </div>
-        </ContainerWidget>
-      </section>
-      <section
-        className="
-          z-50 max-w-[2560px] bg-cover bg-bottom bg-no-repeat 
-          min-h-[1350px] 
-          md:min-h-[1000px]
-          lg:min-h-[1000px]
-          xl:min-h-[1000px] 
-          2xl:min-h-[1000px]
-          3xl:min-h-[1250px]
-          bg-white text-white
-        "
-        style={{
-          backgroundImage: `url(${getS3Url(data?.Image?.url)})`,
-        }}
-      >
-        <ContainerWidget>
-          <div className="flex flex-col items-start justify-start gap-2 md:gap-2">
                 <ScrollWidget animation="fadeUp" delay={0.4}>
               <p className="text-[16px] sm:text-[16px] lg:text-[16px] 
-                xl:text-[16px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
-                font-regular text-black leading-[24px]   md:leading-[26px]
-                w-full md:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px] 
+                xl:text-[14px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
+                font-regular text-black leading-[24px]   md:leading-[20px] lg:leading-[20px] xl:leading-[20px] 3xl:leading-[26px]
+                w-full md:max-w-[650px] xl:max-w-[660px] 2xl:max-w-[850px] 
                 3xl:max-w-[854px] 4xl:max-w-[1100px]">
                 {data.Description[1].children[0].text}
               </p>
             </ScrollWidget>
              <ScrollWidget animation="fadeUp" delay={0.2}>
                     <p className="text-[16px] sm:text-[16px] lg:text-[16px] 
-                xl:text-[16px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
-                font-regular text-black leading-[24px]   md:leading-[26px]
-                w-full md:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px] 
+                xl:text-[14px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
+                font-regular text-black leading-[24px]   md:leading-[20px] lg:leading-[20px] xl:leading-[20px] 3xl:leading-[26px]
+                w-full md:max-w-[650px] xl:max-w-[660px] 2xl:max-w-[850px] 
                 3xl:max-w-[854px] 4xl:max-w-[1100px]">
                 {data.Description[2].children[0].text}
               </p>
             </ScrollWidget>
              <ScrollWidget animation="fadeUp" delay={0.3}>
                     <p className="text-[16px] sm:text-[16px] lg:text-[16px] 
-                xl:text-[16px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
-                font-regular text-black leading-[24px]   md:leading-[26px]
-                w-full md:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px] 
+                xl:text-[14px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
+                font-regular text-black leading-[24px]   md:leading-[20px] lg:leading-[20px] xl:leading-[20px] 3xl:leading-[26px]
+                w-full md:max-w-[650px] xl:max-w-[660px] 2xl:max-w-[850px] 
                 3xl:max-w-[854px] 4xl:max-w-[1100px]">
                 {data.Description[3].children[0].text}
               </p>
             </ScrollWidget>
             <ScrollWidget animation="fadeUp" delay={0.4}>
                     <p className="text-[16px] sm:text-[16px] lg:text-[16px] 
-                xl:text-[16px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
-                font-regular text-black leading-[24px]   md:leading-[26px]
-                w-full md:max-w-[650px] xl:max-w-[750px] 2xl:max-w-[850px] 
+                xl:text-[14px] 2xl:text-[18px] 3xl:text-[18px] 4xl:text-[18px]
+                font-regular text-black leading-[24px]   md:leading-[20px] lg:leading-[20px] xl:leading-[20px] 3xl:leading-[26px]
+                w-full md:max-w-[650px] xl:max-w-[660px] 2xl:max-w-[850px] 
                 3xl:max-w-[854px] 4xl:max-w-[1100px]">
                 {data.Description[4].children[0].text}
               </p>
