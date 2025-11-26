4b23f9a8a6159073706467adacf2c60a4f2ebde1
Pratheesh M <pratheesh@capecomsolutions.com>
2025-11-26 16:09:12 +0530

    updates

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[60vh] leading-8">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-lg">The page you are looking for does not exist.</p>
+      <Link href="/" className="text-[#E97451] hover:text-[#E97451]/80">Go back to the home page</Link>
+    </div>
+  );
+};
+
+export default NotFound;
