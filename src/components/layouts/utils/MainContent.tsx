4506967475f462b414b027fa01f5f13c0de45e2d
Pratheesh M <pratheesh@capecomsolutions.com>
2025-12-01 13:11:47 +0530

    updates

--- src/components/layouts/utils/MainContent.tsx
+++ src/components/layouts/utils/MainContent.tsx
@@ -15,7 +15,7 @@ export default function MainContent({
   return (
     <QueryClientProvider client={queryClient}>
       <main
-        className={`flex-1 w-full ${formPage ? "pt-16" : !isHomePage ? "pt-20 md:pt-24" : ""}`}
+        className={`flex-1 w-full ${formPage ? "pt-16" : !isHomePage ? "pt-17" : ""}`}
       >
         {children}
       </main>
