75766b2e57fa27e7e080ec6f5ef0a7cbd845085e
Pratheesh M <pratheesh@capecomsolutions.com>
2025-12-01 17:05:12 +0530

    updates

--- src/app/more/life-at-lla/page.tsx
+++ src/app/more/life-at-lla/page.tsx
@@ -1,7 +1,6 @@
 import { getLifePageData } from "@/app/api/server";
 import ListSection from "@/components/sections/life-at-lla/LifeLlaSection";
 
-
 const LifeAtLla = async () => {
   const params = { page: 1, per_page: 8 };
   const { data: response } = await getLifePageData(params);
