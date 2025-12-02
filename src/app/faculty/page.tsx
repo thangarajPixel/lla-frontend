66dd9d1cb65024728f22f684433e0f1a0e9640bc
manikandan <manikandan@capecomsolutions.com>
2025-12-02 12:55:50 +0530

    changes

--- src/app/faculty/page.tsx
+++ src/app/faculty/page.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react/jsx-runtime";
 import DirectorAcademicsSection from "@/components/sections/faculty/DirectorAcademicsSection";
 import FacultyHeroSection from "@/components/sections/faculty/FacultyHeroSection";
+import PhotographyFacultySection from "@/components/sections/faculty/PhotographyFacultySection";
 import { getFacultyPageData } from "../api/server";
 
 const Faculty = async () => {
@@ -10,8 +11,7 @@ const Faculty = async () => {
       <Fragment>
         <FacultyHeroSection data={response?.Faculty?.[0]} />
         <DirectorAcademicsSection data={response?.Faculty?.[1]} />
-        {/* <FounderSection data={response?.about?.[1]} />
-        <TeamSection data={response?.about?.[2]} /> */}
+        <PhotographyFacultySection data={response?.Faculty?.[2]} />
       </Fragment>
     );
 };
