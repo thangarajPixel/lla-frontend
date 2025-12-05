5e74143090809f70c9177b4837f4a6bcabb9954d
Pratheesh M <pratheesh@capecomsolutions.com>
2025-12-05 18:24:54 +0530

    updates

--- src/components/widgets/ImageWidget.tsx
+++ src/components/widgets/ImageWidget.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import type { ImageWidgetProps } from "./utils/widget";
 
 const ImageWidget = ({ src, alt, ...props }: ImageWidgetProps) => {
-  return (
-     <Image src={src} alt={alt} {...props} sizes="100vw" />
-  );
+  return <Image src={src} alt={alt} {...props} sizes="100vw" />;
 };
 
 export default ImageWidget;
