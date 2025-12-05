1b997a17b65f352f3efb66f65013de2ebd3c3851
Pratheesh M <pratheesh@capecomsolutions.com>
2025-12-05 12:27:18 +0530

    updates

--- src/components/layouts/WebHeader.tsx
+++ src/components/layouts/WebHeader.tsx
@@ -36,7 +36,7 @@ const menuItems: (MenuItem | DropdownMenuType)[] = [
     items: [
       { href: "/more/about-us", label: "About us" },
       { href: "/more/life-at-lla", label: "Life at LLA" },
-      { href: "/more/blog", label: "Blog" },
+      { href: "/more/blogs", label: "Blog" },
       { href: "/more/contact-us", label: "Contact Us" },
       { href: "/more/faq", label: "FAQ" },
     ],
