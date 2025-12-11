892ce465a5543410b947a16a444c9086f2819850
manikandan <manikandan@capecomsolutions.com>
2025-12-11 18:11:52 +0530

    changes

--- src/components/sections/faculty/DirectorAcademicsSection.tsx
+++ src/components/sections/faculty/DirectorAcademicsSection.tsx
@@ -4,6 +4,7 @@ import OrangeButtonWidget from "@/components/widgets/OrangeButtonWidget";
 import ScrollWidget from "@/components/widgets/ScrollWidget";
 import { getS3Url } from "@/helpers/ConstantHelper";
 import type { FacultyFounderSectionProps } from "./utils/faculty";
+import LinkWidget from "@/components/widgets/LinkWidget";
 
 const DirectorAcademicsSection = ({ data }: FacultyFounderSectionProps) => {
   return (
@@ -33,9 +34,11 @@ const DirectorAcademicsSection = ({ data }: FacultyFounderSectionProps) => {
                 {data?.Founder_card[0]?.Description[2]?.children[0]?.text}
               </p>
               <div className="pt-2 s:pt-2 m:pt-2 xss:pt-2 xs:pt-2 sm:pt-2 md:pt-3 lg:pt-3 xl:pt-3 2xl:pt-3 3xl:pt-4">
+                   <LinkWidget href={`/more/about-us/${data.Founder_card[0].Slug}`}>
                 <OrangeButtonWidget
                   content={data.Founder_card[0].Btn_txt ?? ""}
                 />
+                </LinkWidget>
               </div>
             </div>
           </ScrollWidget>
