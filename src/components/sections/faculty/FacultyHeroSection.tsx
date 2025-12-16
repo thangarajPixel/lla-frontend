4d3cf589f3100483ad5d471fed6a5cd69422e1b4
manikandan <manikandan@capecomsolutions.com>
2025-12-16 14:37:06 +0530

    changes

--- src/components/sections/faculty/FacultyHeroSection.tsx
+++ src/components/sections/faculty/FacultyHeroSection.tsx
@@ -3,6 +3,7 @@ import ImageWidget from "@/components/widgets/ImageWidget";
 import ScrollWidget from "@/components/widgets/ScrollWidget";
 import { getS3Url } from "@/helpers/ConstantHelper";
 import type { FacultyHeroSectionProps } from "./utils/faculty";
+import HTMLWidget from "@/components/widgets/HTMLWidget";
 
 const FacultyHeroSection = ({ data }: FacultyHeroSectionProps) => {
   return (
@@ -14,13 +15,11 @@ const FacultyHeroSection = ({ data }: FacultyHeroSectionProps) => {
               <h2 className="font-urbanist  font-regular font-normal text-black text-[32px] sm:text-[36px] md:text-[36px] lg:text-[48px] xl:text-[48px] 2xl:text-[60px] 3xl:text-[64px]">
                 {data.Title}
               </h2>
-              <p
+              <HTMLWidget content={data.Heading}
                 className="text-[24px] sm:text-[24px] md:text-[20px] lg:text-[22px] xl:text-[24px] 2xl:text-[30px] 3xl:text-[40px]
                font-regular font-mulish  text-black w-full sm:max-w-[320px] md:max-w-[375px] lg:max-w-[375px] xl:max-w-[440px] 2xl:max-w-[550px] 3xl:max-w-[606px] 3xl:leading-[48px]"
-              >
-                {data.Heading}
-                <span className="text-[#E97451] ml-2"> {data.SubHeading} </span>
-              </p>
+                tag="p"
+              />
               <p className="text-left mt-4  font-mulish text-black text-[18px] sm:text-[14px] md:text-[17px] 3xl:text-[18px] w-full sm:max-w-[320px] md:max-w-[375px] lg:max-w-[375px] xl:max-w-[430px] 2xl:max-w-[555px] 3xl:max-w-[606px]">
                 {data.Description}
               </p>
