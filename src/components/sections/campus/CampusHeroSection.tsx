5bdd1ab7165de9a46aba3cdb7aa642a7343f6db6
manikandan <manikandan@capecomsolutions.com>
2025-12-17 15:07:16 +0530

    changes

--- src/components/sections/campus/CampusHeroSection.tsx
+++ src/components/sections/campus/CampusHeroSection.tsx
@@ -8,6 +8,7 @@ import ContainerWidget from "@/components/widgets/ContainerWidget";
 import { getS3Url } from "@/helpers/ConstantHelper";
 import { Into, Play } from "@/helpers/ImageHelper";
 import type { CampusHeroSectionProps } from "./utils/campus";
+import ScrollWidget from "@/components/widgets/ScrollWidget";
 
 const CampusHeroSection = ({ data }: CampusHeroSectionProps) => {
   const stopAllVideos = () => {
@@ -34,58 +35,10 @@ const CampusHeroSection = ({ data }: CampusHeroSectionProps) => {
           <track kind="captions" srcLang="en" label="English" />
         </video>
       </div>
-      <div className="absolute inset-0 bg-black/30 z-[1]" />
-      <DialogWidget
-        trigger={
-          <div className="absolute inset-0 flex items-center justify-center z-10   cursor-pointer">
-            <div className="video-main">
-              <div className="waves-block">
-                <div className="waves wave-1" />
-                <div className="waves wave-2" />
-                <div className="waves wave-3" />
-              </div>
-            </div>
-            <ButtonWidget
-              type="button"
-              className="relative w-15 h-15 p-0  bg-transparent hover:bg-transparent border-none shadow-none rounded-full group/play-button z-10"
-            >
-              <ImageWidget
-                src={Play}
-                alt="play video"
-                className="w-13 h-13 cursor-pointer transition-colors duration-300 group-hover/play-button:text-[#E97451]"
-              />
-            </ButtonWidget>
-          </div>
-        }
-        contentClassName="sm:max-w-[90vw] lg:max-w-[800px] p-0"
-        showCancel={false}
-        showCloseButton={false}
-        onOpenChange={(open) => {
-          if (!open) stopAllVideos();
-        }}
-        customCloseButton={
-          <DialogClose asChild>
-            <div className="cursor-pointer -mt-[30px] -mr-[30px]">
-              <ImageWidget src={Into} alt="Close" className="w-[30px] h-[30px]" />
-            </div>
-          </DialogClose>
-        }
-      >
-        <div className="relative w-full aspect-video bg-black rounded-lg">
-          <video
-            src={data?.Video?.url ? getS3Url(data.Video.url) : ""}
-            autoPlay
-            loop
-            muted
-            playsInline
-            controls
-            className="w-full h-full object-contain rounded-lg"
-          />
-        </div>
-      </DialogWidget>
       <ContainerWidget >
-        <div className="grid grid-cols-1  gap-0 xl:py-40 3xl:py-45 relative z-10">
-          <div className="flex flex-col items-start bg-black/30 p-4  backdrop-blur-none xl:max-w-[550px] 3xl:max-w-[740px] 3xl:max-h-[662px] ">
+         <ScrollWidget animation="fadeUp" delay={0.1}>
+        <div className="grid grid-cols-1  gap-0 py-20 xl:py-40 3xl:py-40 relative z-10">
+          <div className="flex flex-col items-start bg-black/30 p-4  backdrop-blur-none md:max-w-[500px] lg:max-w-[500px] xl:max-w-[550px] 2xl:max-w-[640px] 3xl:max-w-[740px] 3xl:max-h-[662px] ">
             {data?.Title && (
               <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl xl:text-[50px] 2xl:text-[60px] 3xl:text-[64px] font-urbanist  font-normal mb-3 xl:max-w-[300px] 3xl:max-w-[500px]">
                 {data.Title}
@@ -97,22 +50,23 @@ const CampusHeroSection = ({ data }: CampusHeroSectionProps) => {
               </p>
             )}
             <div className="flex flex-col gap-4 mt-6 max-w-[692px]">
-             {data.Description?.map((paragraph, index) => {
-              const textContent = paragraph.children?.[0]?.text || "";
-              if (!textContent) return null;
-              const uniqueKey = `${index}-${textContent.slice(0, 20)}`;
-              return (
-                <p
-                  key={uniqueKey}
-                  className="font-mulish text-sm xss:text-[16px] sm:text-base lg:text-[17px] xl:text-[14px] 2xl:text-[17px] 3xl:text-[18px] font-normal text-white xl:leading-[24px] 3xl:leading-[26px]"
-                >
-                  {textContent}
-                </p>
-              );
-            })}
-             </div>
+              {data.Description?.map((paragraph, index) => {
+                const textContent = paragraph.children?.[0]?.text || "";
+                if (!textContent) return null;
+                const uniqueKey = `${index}-${textContent.slice(0, 20)}`;
+                return (
+                  <p
+                    key={uniqueKey}
+                    className="font-mulish text-sm xss:text-[16px] sm:text-base lg:text-[17px] xl:text-[14px] 2xl:text-[17px] 3xl:text-[18px] font-normal text-white xl:leading-[24px] 3xl:leading-[26px]"
+                  >
+                    {textContent}
+                  </p>
+                );
+              })}
+            </div>
           </div>
         </div>
+        </ScrollWidget>
       </ContainerWidget>
     </section>
   );
