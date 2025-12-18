daa1b813d44f898e79f0474809ffa1121c1bbb72
manikandan <manikandan@capecomsolutions.com>
2025-12-18 10:09:39 +0530

    changes

--- src/app/more/blogs/page.tsx
+++ src/app/more/blogs/page.tsx
@@ -2,7 +2,7 @@ import { getBlogPageData } from "@/app/api/server";
 import BlogSection from "@/components/sections/more/blogs/BlogSection";
 
 const Blogs = async () => {
-  const params = { page: 1, per_page: 6 };
+  const params = { page: 1, per_page: 9 };
   const { data: response } = await getBlogPageData(params);
   if (response) return <BlogSection data={response} />;
 };
