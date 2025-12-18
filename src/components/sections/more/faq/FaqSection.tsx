f37cdd38eeca6eedcf30532c4720020084e8e2ff
manikandan <manikandan@capecomsolutions.com>
2025-12-18 17:50:44 +0530

    changes

--- src/components/sections/more/faq/FaqSection.tsx
+++ src/components/sections/more/faq/FaqSection.tsx
@@ -81,8 +81,8 @@ const FaqSection = ({ data }: FaqProps) => {
             {data.Title || "Frequently Asked Questions"}
           </h1>
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-14">
-            <div className="hidden md:block lg:col-span-1 shadow-xl ">
-              <nav className="space-y-1 lg:sticky lg:top-30">
+            <div className="hidden md:block lg:col-span-1   ">
+              <nav className="space-y-1 lg:sticky lg:top-30 shadow-lg">
                 {data.faq.map((category) => (
                   <button
                     key={category.id}
