0b261291b340c848e15f31cecddff3965e37c594
Ajith M <ajith.m@capecomsolutions.com>
2026-01-16 12:58:24 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -42,7 +42,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: '/courses/pg-diploma-in-professional-photography-digital-production-2025',
-        destination: '/courses/pg-diploma-in-professional-photography-videography',
+        destination: '/',
         permanent: true,
       },
       {
