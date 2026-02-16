bf94e03ed1826ba21345ab09bf84230f98302821
Ajith M <ajith.m@capecomsolutions.com>
2026-02-16 11:45:21 +0530

    script add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default async function RootLayout({
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "lr881zkmpm");`}
+            })(window, document, "clarity", "script", "vgqxd0deyn");`}
         </Script>
 
         {/* Facebook Pixel */}
