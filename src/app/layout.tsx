924ac78b20158abf5b84afde1eaac8fdf06c1dc0
Ajith M <ajith.m@capecomsolutions.com>
2026-02-27 18:18:05 +0530

    Clarity script hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,13 +77,13 @@ export default async function RootLayout({
         }} />
 
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        {/* <Script id="clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
           __html: `(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];if(y && y.parentNode) y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "vgqxd0deyn");`
-        }} />
+        }} /> */}
 
         {/* Facebook Pixel */}
         <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
