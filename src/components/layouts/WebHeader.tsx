ff7a811427862b62d8fefdbdddeb7a8ed120b4bf
Rithish Abinav - Pixel <rithish.pixel@gmail.com>
2026-03-21 10:43:30 +0530

    Update WebHeader.tsx

--- src/components/layouts/WebHeader.tsx
+++ src/components/layouts/WebHeader.tsx
@@ -155,6 +155,8 @@ const WebHeader = ({
   ): item is DropdownMenuType => "items" in item;
 
   return (
+    <>
+    <div id="celebration_topbar"><a href="https://llacademy.com/" target="_blank">Watch Live Streaming of 25th Anniversary Celebrations</a></div>
     <header
       className={`w-full z-50 transition-all duration-500
         ${isHidden ? "-translate-y-full" : "translate-y-0"}
@@ -221,6 +223,7 @@ const WebHeader = ({
         </ContainerWidget>
       </nav>
     </header>
+    </>
   );
 };
 
